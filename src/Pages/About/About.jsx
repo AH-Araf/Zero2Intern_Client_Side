386c3211e02216d334dc78a5c8572aae89f608bf
Sumiya Jakariya <sumiyajakariya38299@gmail.com>
2023-12-06 22:34:50 +0600

    newly added

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -3,7 +3,12 @@
 const About = () => {
     return (
         <div>
-            <h1>About</h1>
+            <h1 className="About_Heading">About</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui nihil quod in eum sapiente accusantium velit aut animi modi nam, ratione aperiam officia quidem fuga aspernatur consectetur error minus doloremque!
+            Magni ducimus sequi ut dolor minus, iure voluptate nostrum delectus fuga error? Laudantium dolor odio alias quasi sint, officia dolorem. Eligendi aperiam tempore debitis vel quae recusandae quibusdam? Facilis, veritatis?
+            Numquam quam amet rem odio velit, architecto totam dignissimos tempora adipisci accusamus explicabo in sapiente ipsam quis impedit ducimus vero nisi natus veniam exercitationem, incidunt quasi. Fuga ipsam consequuntur veniam.
+            Hic, consectetur? Est voluptatibus obcaecati quam, impedit pariatur at culpa repellendus iste rem eveniet eius a nostrum. Ratione vel reprehenderit dolore eveniet vero similique quaerat dicta magnam, dolorum, veritatis quis.
+            Odio dolore officia rem inventore recusandae repellat, corporis nisi saepe iure quidem et amet consequuntur dolorum sit doloribus esse, ea ab, aliquid illum. Porro, alias. Veniam dolor voluptatem fuga odio.</p>
         </div>
     );
 };
